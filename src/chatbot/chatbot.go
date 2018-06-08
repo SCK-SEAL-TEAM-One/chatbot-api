2571b5d7407a905e59e7b32cf110d5c476f895c6
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-06-08 14:56:17 +0700

    [Created] สร้างไฟล์ chatbot.go

---
+++ src/chatbot/chatbot.go
@@ -0,0 +1,28 @@
+package chatbot
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type answerMessage struct {
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+}
+
+func Chatbot(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	companyID := r.PostFormValue("company_id")
+	message := r.PostFormValue("message")
+	timestamp := r.PostFormValue("timestamp")
+
+	if companyID == "1234" && message == "หอพักย่านลาดพร้าวซอย1" && timestamp == "2018-08-12T 03:59:58.855Z" {
+		answerMessageStruct := answerMessage{
+			Message:   "ละออเพลส,เรือนสุดสุข,ละอ่อน",
+			Timestamp: "2018-08-12T 03:59:59.855Z",
+		}
+		responseMessage, _ := json.Marshal(answerMessageStruct)
+		w.Write(responseMessage)
+	}
+
+}
---
+++ src/chatbot/chatbot_test.go
@@ -0,0 +1,24 @@
+package chatbot
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func Test_Chatbot_Input_company_id_1234_And_message_And_Timestamp_2018_08_12T_03_59_58_Dot_855Z(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/chatbot/api/message", strings.NewReader("company_id=1234&message=หอพักย่านลาดพร้าวซอย1&timestamp=2018-08-12T 03:59:58.855Z"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	expected := `{"message":"ละออเพลส,เรือนสุดสุข,ละอ่อน","timestamp":"2018-08-12T 03:59:58.855Z"}`
+
+	Chatbot(res, req)
+	resp := res.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if expected != string(body) {
+		t.Errorf("should be %s but got %s", expected, string(body))
+	}
+
+}
